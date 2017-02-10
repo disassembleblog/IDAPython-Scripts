1f171683264990a18869b72800c7d017b9330a87
icspe <cristian.iiacob@gmail.com>
2017-02-10 11:48:04 +0200

    Added Comments

--- BPs.py
+++ BPs.py
@@ -2,8 +2,9 @@
 Author: 		Ionut Iacob
 Functionality: 	Add breakpoints in IDA on the functions from the bpFuncs list.
 At the end of the script, a summary is printed on the output of IDA.
-Aate: 			20.12.2016
-Version			0.2
+Date: 			20.1.2017
+Version			0.22
+to to:	bp "call eax", call to a function with an abnormal ret or jmp (red)
 '''
 from idaapi import *
 import idc
@@ -12,29 +13,29 @@ import idautils
 print("\n")*10
 
 # pentru a evita duplicate de genul CreateProcessW si CreateProcessA, ar trebui parsat tabela de IAT si daca de acolo se potriveste CreateProcess pe tabela, breakpoint
-bpFuncs = ["WriteFile" ,"GetCurrentProcess", "CreateEventW", "WriteProcessMemory" , "VirtualAlloc" , "VirtualProtect" , "SetSecurityDescriptorDacl" , "ResumeThread" , "RegSetValueExA" , "Process32First" , "Process32Next" , "OpenProcessToken" , "LookupPrivilegeValueA" , "LoadResource" , "LockResource" , "GetProcAddress" , "LoadLibraryExA" , "LoadLibraryA" , "GetWindowsDirectoryA" , "GetTickCount" , "GetTempPathA" , "GetSystemTime" , "GetDriveTypeA" , "FindFirstFileA" , "FindNextFileA" , "EnumWindows" , "DeleteFileA" , "CreateToolhelp32Snapshot" , "CreateThread" , "CreateProcessA" ,"CreateProcessInternalW" ,"CreateProcessW" , "CreateFileW" , "CreateFileA" , "CopyFileA", "AdjustTokenPrivileges", "LookupPrivilegeValueW", "RegDeleteKeyExW", "OpenClipboard", "SetClipboardData", "InternetCrackUrl", "InternetConnect", "HttpOpenRequest", "HttpSendRequest", "InternetReadFile", "InternetOpen", "WSAStartup", "socket", "bind", "listen", "accept", "recv", "shutdown", "getaddrinfo", "connect", "send"]
-notFound = []	# functii negasite
-objFoundFuncs = [] # lista pentru functii gasite
+bpFuncs = ["WriteFile" ,"GetCurrentProcess", "CreateEventW", "WriteProcessMemory" , "VirtualAlloc" , "VirtualProtect" , "SetSecurityDescriptorDacl" , "ResumeThread" , "RegSetValueExA" , "Process32First" , "Process32Next" , "OpenProcessToken" , "LookupPrivilegeValueA" , "LoadResource" , "LockResource" , "GetProcAddress" , "LoadLibraryExA" , "LoadLibraryA" , "GetWindowsDirectoryA" , "GetTickCount" , "GetTempPathA" , "GetSystemTime" , "GetDriveTypeA" , "FindFirstFileA" , "FindNextFileA" , "EnumWindows" , "DeleteFileA" , "CreateToolhelp32Snapshot" , "CreateThread" , "CreateProcessA" ,"CreateProcessInternalW" ,"CreateProcessW" , "CreateFileW" , "CreateFileA" , "CopyFileA", "AdjustTokenPrivileges", "LookupPrivilegeValueW", "RegDeleteKeyExW", "OpenClipboard", "SetClipboardData", "InternetCrackUrl", "InternetConnect", "HttpOpenRequest", "HttpSendRequest", "InternetReadFile", "InternetOpen", "WSAStartup", "socket", "bind", "listen", "accept", "recv", "shutdown", "getaddrinfo", "connect", "send", "ShellExecuteExW", "CryptDecrypt", "CryptEncrypt", "CryptAcquireContextW" ,"CryptImportKey"]
+notFound = []				# unfound functions
+objFoundFuncs = []		 	# found functions
 
-for bpFunc in bpFuncs:
-	addr = LocByName(bpFunc) 
-	if addr == BADADDR:
-		notFound.append(bpFunc)
-	else:
-		if len(list(XrefsTo(addr,0)))==0:
-			notFound.append(bpFunc)
-		for xref in XrefsTo(addr, 0):
-			buff = GetMnem(xref.frm)
-			if xref.type in (16,17):	#Code_Near_Call
-				objFoundFuncs.append(xref)
-				#print("{} {} from, {:08X} to {:08X}".format(xref.type, XrefTypeName(xref.type), xref.frm, xref.to))
+for bpFunc in bpFuncs:						# iterate through each function we want to breakpoint
+	addr = LocByName(bpFunc) 				# check if the func is present in the IAT of the PE
+	if addr == BADADDR:						# if no addr was found
+		notFound.append(bpFunc)				# add unfound functions to list
+	else:									# 
+		if len(list(XrefsTo(addr,0)))==0:	# search for all xrefs to that addr, make it a list and get a list lenght
+			notFound.append(bpFunc)			# if list is 0 add function to unfound list
+		for xref in XrefsTo(addr, 0):		# iterate through all xrefs
+			buff = GetMnem(xref.frm)		# get the mnemonic
+			if xref.type in (16,17):		# check if mnem is a call
+				objFoundFuncs.append(xref)	# add addr to breakpoint array
+				MakeComm(xref.frm, bpFunc)	# add comment
 
 for objFoundFunc in objFoundFuncs:
 	idc.AddBpt(objFoundFunc.frm)
 
-print("Found and set a no. of {} BPs.".format(len(objFoundFuncs)))
-print("These functions where not found: {}/{} -> {}".format(len(notFound),len(bpFuncs),notFound))
-
+#print("Found and set a no. of {} BPs.".format(len(objFoundFuncs)))
+#print("These functions where not found: {}/{} -> {}".format(len(notFound),len(bpFuncs),notFound))
+print("Breakpoint script finished. Check breakpoint window.")
 
 
 #  XREF TYPES
