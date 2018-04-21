e675943ff5d7bff3f117041d4d628be4f54f4155
icspe <cristian.iiacob@gmail.com>
2018-04-21 16:55:26 +0300

    Add files via upload

---
+++ decodeStringsBochs.py
@@ -0,0 +1,61 @@
+'''
+#### THIS SCRIPT IS AN EXAMPLE OF HOW TO DEOBFUSCATE STRING USING A FUNCTION LIKE THE PROTOTYPE FROM LINE 5
+create a proper function type and name in you IDB
+set the "fDecodeString1" to you function name at lines: -15- and -50-
+Ex: int ___cdecl decode1 (char*, char*, int)
+then enable bochs debugger, check "stop on start" and have it set to "IDB" instead of "PE" debugging
+ready to go! 
+'''
+
+import idaapi
+import idc
+import idautils
+
+#get address of the decoding function
+poiFunc = LocByName('fDecodeString1')
+
+if not poiFunc:
+	exit("KKT")
+#get all refeerences to that function
+xrefsDec1 = CodeRefsTo(poiFunc,1)
+
+#iterate though each reference
+for xref in xrefsDec1:
+
+	def GetStrLen(instr):
+		count = 0
+		while(Byte(instr+count)):
+			count += 1
+		return count
+
+	pushes = []
+	instr = PrevHead(xref,SegStart(xref))
+	
+	while(len(pushes)<3):
+		if(GetMnem(instr)=='push'):
+			pushes.append(instr)
+		instr = PrevHead(instr,SegStart(xref))
+
+	if(len(pushes)!=3):
+		print("ERROR BOSS")
+
+	encodedStr = GetOperandValue(pushes[0],0)
+	encodedLen = GetOperandValue(pushes[2],0)
+	print('Decoding string {:08X}'.format(pushes[0]))
+	if(encodedLen==0):
+		try:
+			#print('No len for string from addr {:08X}'.format(pushes[0]))
+			encodedLen = GetStrLen(encodedStr)
+		except:
+			encodedLen = AskLong(0,'Lenght of {:08X}'.format(encodedStr))
+			if(encodedLen<=0):
+				print('Skipping {:08X}'.format(encodedStr))
+				continue
+
+	decodedStr = Appcall.buffer(" ",encodedLen)
+
+	
+	Appcall.fDecodeString1(encodedStr,decodedStr,encodedLen)
+	MakeComm(xref,decodedStr.value)
+	print(decodedStr.value)
+print('FIN')
---
+++ decodeStringsStatically.py
@@ -0,0 +1,52 @@
+import idaapi, idc, idautils
+
+funcName = LocByName('fDecodeString1')
+
+funcXrefs = CodeRefsTo(funcName,1)
+
+for xref in funcXrefs:
+	print("Function referenced from address: {:08X}".format(xref))
+
+	def GetStrLen(instr):
+		count = 0
+		while(Byte(instr+count)):
+			count += 1
+		return count
+
+
+	prevInstr = []
+	while(len(prevInstr)==0):
+		temp = PrevHead(xref,SegStart(xref))
+		if(GetMnem(temp)=='push'):
+			prevInstr.append(temp)
+			continue
+		temp = PrevHead(temp,SegStart(xref))
+
+	prevInstr = prevInstr[0]
+
+	try:
+		encodedStr = GetOperandValue(prevInstr,0)
+		encodedLen = GetStrLen(encodedStr)
+	except:
+		print('Can\'t get string from address: {:08X}'.format(prevInstr))
+		Jump(encodedStr)
+		encodedLen = AskLong(0,'String lenght @ {:08X}'.format(encodedStr))
+		if(encodedLen==0):
+			print('No lenght given, skipping string.')
+
+	#print('String: {:08X} @ {}'.format(encodedStr,encodedLen))
+	encBytes = []
+	for iter in range(encodedLen):
+		#print("0x{:02X}".format(Byte(encodedStr+iter)))
+		encBytes.append(Byte(encodedStr+iter))
+
+	decBytes = []
+
+	for byte in encBytes:
+		buff = byte ^ 0x41
+		buff -= 10
+		decBytes.append(chr(buff))
+
+	print(''.join(decBytes))
+	MakeComm(xref,''.join(decBytes))
+
